5acc1612e3af2f65c598f691783a82b7b707a4bc
sksayril <sksayril123@gmail.com>
2025-11-05 15:23:52 +0530

    fixed the problem also

--- src/components/society/PaymentHistoryContent.tsx
+++ src/components/society/PaymentHistoryContent.tsx
@@ -216,7 +216,7 @@ const PaymentHistoryContent: React.FC = () => {
           onClick={() => setFilters({ page: 1, limit: 10 })}
           className="px-4 py-2 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200 transition-colors"
         >
-          Clear Filters
+          Clear Filter
         </button>
         <button
           onClick={fetchPaymentHistory}
